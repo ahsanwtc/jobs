cb96a8f00f8ec2a092cfce49cae32bff8ddf0932
jßan <ahsan.wtc@gmail.com>
2018-03-26 23:42:01 +0200

    adding navigation to app

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 export default class App extends React.Component {
-  	render() {
+      
+    render() {
+        const MainNavigator = new TabNavigator({
+            welcome: { screen: WelcomeScreen },
+            auth: { screen: AuthScreen },
+            main: {
+                screen: TabNavigator({
+                    map: { screen: MapScreen },
+                    deck: { screen: DeckScreen },
+                    review: {
+                        screen: StackNavigator({
+                            review: { screen: ReviewScreen },
+                            settings: { screen: SettingsScreen }
+                        })
+                    }
+                }, {
+                    tabBarPosition: "bottom",
+                    tabBarOptions: {
+                        indicatorStyle: {
+                            backgroundColor: "blue"
+                        }
+                    }
+                })
+            }
+        }, {
+            tabBarPosition: "bottom",
+            tabBarOptions: {
+                indicatorStyle: {
+                    backgroundColor: "blue"
+                }
+            }
+        });
     	return (
+
     		<View style={styles.container}>
-        		<Text>Open up App.js to start working on your app!</Text>
+				<MainNavigator />
       		</View>
     	);
   	}
@@ -15,7 +55,8 @@ const styles = StyleSheet.create({
   	container: {
     	flex: 1,
     	backgroundColor: '#fff',
-    	alignItems: 'center',
-    	justifyContent: 'center',
+    	// alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: Platform.OS === 'android' ? Expo.Constants.statusBarHeight : undefined
   	},
 });
